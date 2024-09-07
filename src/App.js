a6ecc9773267638970f4596ea19ef81b299536f0
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-09-07 17:08:19 +0300

    feat: add routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { Suspense } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/not-found/NotFound";
+import LoadingPage from "./pages/LoadingPage";
+import MainLayout from "./components/Layouts/MainLayout";
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense fallback={<LoadingPage />}>
+              <Home />
+            </Suspense>
+          ),
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
+      <RouterProvider router={router} />;
     </div>
   );
 }
