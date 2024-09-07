635d3c22bc49311deb206d63eefe70d41008d37e
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-09-07 17:05:29 +0300

    feat: add footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        made with love by{" "}
+        <a
+          href="https://www.linkedin.com/in/abdelrahman-elzayat-6989b221a/"
+          target="_blank"
+        >
+          abdelrahman elzayat
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
